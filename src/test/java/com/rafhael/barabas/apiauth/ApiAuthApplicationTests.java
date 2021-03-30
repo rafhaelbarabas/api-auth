96a39b879bd4ad1d867f9464f1918a0902dd4b9a
Rafhael de Andrade Barabas <rafhael_ab@hotmail.com>
2021-03-30 14:41:45 -0300

    initial
    initial

---
+++ src/test/java/com/rafhael/barabas/apiauth/ApiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafhael.barabas.apiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
